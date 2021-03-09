0aca15629fd5e95bfe9fa29288a316a0f5a7ee63
valekask <alekask.v@gmail.com>
2021-03-09 18:01:37 +0200

    update map scaling

--- js/map.utils.js
+++ js/map.utils.js
@@ -2,7 +2,6 @@ function getMapScale() {
     const map = [chart.originalSize.width, chart.originalSize.height];
     const box = [chart.plotWidth, chart.plotHeight];
     let k = 1;
-    let scaleName = "";
     let margin = [0, 0];
 
     // calculate diff
@@ -13,33 +12,25 @@ function getMapScale() {
     if (kw < 0 || kh < 0) {
         const kwAbs = Math.abs(kw);
         const khAbs = Math.abs(kh);
-        if (kwAbs > khAbs) {
-            k = box[1] / map[1];
-            scaleName = "zoomout-h";
-
-            const scaledSide = map[0] * k;
-            const center = (box[0] - scaledSide) / 2;
-            margin[0] = Math.round(center / k);
-        } else {
+        if (kw < 0 && kwAbs > khAbs) { // scale out width
             k = box[0] / map[0];
-            scaleName = "zoomout-w";
-
             const scaledSide = map[1] * k;
             const center = (box[1] - scaledSide) / 2;
             margin[1] = Math.round(center / k);
-        }
-    } else {
-        if (kw > kh) {
+        } else { // scale out height
             k = box[1] / map[1];
-            scaleName = "zoomin-h";
-
             const scaledSide = map[0] * k;
             const center = (box[0] - scaledSide) / 2;
             margin[0] = Math.round(center / k);
-        } else {
+        }
+    } else {
+        if (kw > kh) { // scale in height
+            k = box[1] / map[1];
+            const scaledSide = map[0] * k;
+            const center = (box[0] - scaledSide) / 2;
+            margin[0] = Math.round(center / k);
+        } else {  // scale in width
             k = box[0] / map[0];
-            scaleName = "zoomin-w";
-
             const scaledSide = map[1] * k;
             const center = (box[1] - scaledSide) / 2;
             margin[1] = Math.round(center / k);
